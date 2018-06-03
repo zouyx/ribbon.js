79586a9bd70260f1dca00ad7c33ca1afb6b05a75
joezou <yixian.zou@gmail.com>
2018-06-03 22:49:47 +0800

    update

--- src/ribbon.js
+++ src/ribbon.js
@@ -1,19 +1,17 @@
-
-export default class Ribbon{
-    constructor() {
-
+function chooseServerUrl(servers) {
+    var choosenServer = chooseServer(servers);
+    if (!choosenServer) {
+        return;
     }
 
-    chooseServerUrl(servers) {
-        var choosenServer = chooseServer(servers);
-        if(!choosenServer){
-            return;
-        }
+    return choosenServer.homePageUrl;
+}
 
-        return choosenServer.homePageUrl;
-    }
+function chooseServer(servers) {
+    return servers[0]
+}
 
-    chooseServer(servers) {
-        return servers[0]
-    }
+module.exports ={
+    chooseServerUrl:chooseServerUrl,
+    chooseServer:chooseServer
 }
