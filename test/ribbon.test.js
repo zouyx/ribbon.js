0d3e1d5e9f511dbcf5cca822cac9575a503eafb1
joezou <yixian.zou@gmail.com>
2018-06-03 23:10:48 +0800

    update

--- test/ribbon.test.js
+++ test/ribbon.test.js
@@ -6,18 +6,41 @@ var assert = chai.assert
 
 var serversJson=""
 
+const Eureka = require('eureka-js-client').Eureka;
+
+// example configuration
+const client = new Eureka({
+    // application instance information
+    instance: {
+        app: 'jqservice',
+        hostName: 'localhost',
+        ipAddr: '127.0.0.1',
+        port: 8080,
+        vipAddress: 'jq.test.something.com',
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn',
+        },
+    },
+    eureka: {
+        // eureka server host / port
+        host: 'localhost',
+        port: 8761,
+    },
+});
+
 
 describe('ribbon', function() {
     describe('#chooseServer()', function() {
         it('should return choose server', function() {
-            var servers = JSON.parse(serversJson);
-            var ribbonInstance=new Ribbon()
-            var value=ribbonInstance.chooseServer(servers)
+            client.start();
+            var servers = client.getInstancesByAppId('STORE-SERVICE');
+            var value=Ribbon.chooseServer(servers)
 
             // assert(Array.isArray(value))
             assert.isNotNaN(value)
             assert.isString(value.app)
-            assert.equal(value.app,'OCTOPUS-STORE-SERVICE')
+            assert.equal(value.app,'STORE-SERVICE')
         });
     });
 
