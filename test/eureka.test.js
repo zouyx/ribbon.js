eed430f78aa93044ae6021805863e707bcc2fc88
joe01.zou <joe01.zou@vipshop.com>
2018-06-06 15:29:42 +0800

    update case

---
+++ test/eureka.test.js
@@ -0,0 +1,34 @@
+const Eureka = require('eureka-js-client').Eureka;
+// import Eureka from 'eureka-js-client';
+
+// example configuration
+const client = new Eureka({
+    // application instance information
+    instance: {
+        app: 'BAIDU-SERVICE',
+        hostName: 'baidu.com',
+        ipAddr: '10.199.204.7',
+        vipAddress: 'baidu.com',
+        statusPageUrl: 'http://localhost:9101',
+        port: {
+            '$': 9101,
+            '@enabled': 'true',
+        },
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn',
+        },
+    },
+    eureka: {
+        // eureka server host / port
+        host: 'localhost',
+        port: 8761,
+        servicePath:'/eureka/apps/'
+    },
+});
+
+client.start();
+
+module.exports ={
+    client:client,
+}
